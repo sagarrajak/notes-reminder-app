bb453d3036d856b900e37dc7bcd11b7797679de4
sagarrajak <sagarrajak858@gmail.com>
2018-09-03 21:49:20 -0400

    Added logger

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const Koa =  require('koa');
 const dotenv = require('dotenv');
 const http = require('http');
+const logger = require('./src/logger/logger');
 const app =  new Koa();
 
-process.env.NODE_ENV = 'production';  //SET environment variable here in file
+process.env.NODE_ENV = require('./config.js');  //SET environment variable here in file
 switch (process.env.NODE_ENV) {
     case 'development':
         dotenv.config({path: '.dev.env'});
@@ -15,7 +16,7 @@ switch (process.env.NODE_ENV) {
         dotenv.config({path: '.test.env'});
         break; 
 }
-const db = require('./database');
+const db = require('./database'); //Database object for query and all 
 
 app.use(async (ctx) => {
     ctx.body = {
@@ -27,6 +28,7 @@ app.use(async (ctx) => {
 const PORT = process.env.PORT||8080;
 const server = http.createServer(app.callback());
 server.listen(process.env.PORT||8080, () => {
-    console.log(`server is listing at ${PORT}`);
+    // console.log(`server is listing at ${PORT}`);
+    logger.log('info', 'server is listening at %d', PORT);
 });
 module.exports = server;
---
+++ src/logger/logger.js
@@ -0,0 +1,46 @@
+const {createLogger, format, transports} = require('winston');
+const {timestamp, combine} = format;
+ /** 
+  *const levels = { 
+        error: 0, 
+        warn: 1, 
+        info: 2, 
+        verbose: 3, 
+        debug: 4, 
+        silly: 5 
+    };
+*/
+const loggerDevelopment = createLogger({
+    format: combine(
+        timestamp(),
+        format.splat(),
+        format.simple()
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({filename: 'combined.log'})
+    ],
+    exitOnError: true,  // In development environment process should exit after error
+    level: 'verbose'     //Log for verbose and above                   
+});
+
+const loggerProduction = createLogger({
+    format: combine(
+        timestamp(),
+        format.splat(),
+        format.simple()
+    ),
+    transports: [
+        new transports.File({filename: 'production.log'}),
+        new transports.File({filename: 'error.log', level: 'error'}),
+    ],
+    exitOnError: false,
+    level: 'info',    //error for warning and above for 'proudction' environment,
+});
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports = loggerProduction;    
+} else {
+    module.exports = loggerDevelopment;
+}
+
